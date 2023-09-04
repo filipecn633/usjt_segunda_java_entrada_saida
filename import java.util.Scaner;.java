b7cd43fcce90825eb6be1666292deb2a6d28a957
Filipe Nascimento <filipecn633@gmail.com>
2023-09-04 19:44:56 -0300

    ap20(input-output): declaracao de variaveis

---
+++ import java.util.Scaner;.java
@@ -0,0 +1,16 @@
+import java.util.Scaner;
+//pacote padrão: java.lang: importado automaticamente
+public class SomaDoisNumeros{
+ public static void main(String [] args){
+        //declaração de variáveis
+        Scanner leitor = new Scanner(System.in);
+        double primeiroOperando;
+        double segundoOperando;
+        double resultado;
+
+        //entrada 
+
+        //processamento
+
+        //saída
+    }
